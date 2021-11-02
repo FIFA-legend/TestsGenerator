b161f4753f236cfaa7ed93496206cb45784e63e4
FIFA-legend <58667610+FIFA-legend@users.noreply.github.com>
2021-11-02 10:57:52 +0300

    Add files via upload

---
+++ TestsGeneratorLib/TestUnit.cs
@@ -0,0 +1,14 @@
+﻿namespace TestsGeneratorLib
+{
+    public class TestUnit
+    {
+        public string FileName { get; }
+        public string SourceCode { get; }
+
+        internal TestUnit(string fileName, string sourceCode)
+        {
+            FileName = fileName;
+            SourceCode = sourceCode;
+        }
+    }
+}
